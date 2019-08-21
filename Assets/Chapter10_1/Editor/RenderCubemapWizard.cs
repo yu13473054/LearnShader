85f37af13ad878c95f13b18e997a43c3f9e5a9ca
yu13473054 <849460181@qq.com>
2019-08-21 18:30:24 +0800

    11111

---
+++ Assets/Chapter10_1/Editor/RenderCubemapWizard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RenderCubemapWizard : ScriptableWizard
+{
+    public Transform posTrans;
+    public Cubemap cubemap;
+
+    private void OnWizardCreate()
+    {
+        GameObject go = new GameObject("CubeMapCamera");
+        Camera cam = go.AddComponent<Camera>();
+        go.transform.position = posTrans.position;
+        cam.RenderToCubemap(cubemap);
+        DestroyImmediate(go);
+    }
+
+    private void OnWizardUpdate()
+    {
+        helpString = "选择需要渲染的Transform和Cubemap！";
+        isValid = posTrans && cubemap;
+    }
+
+    [MenuItem("Tools/Render cubemap")]
+    static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard<RenderCubemapWizard>("Render cubemap", "render", "aa");
+    }
+}
